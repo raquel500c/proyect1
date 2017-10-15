914fab81a1ef128e72b082fde4fa10d757f47b6d
raquel500c <raquel500c@gmail.com>
2017-10-15 15:50:59 +0200

    position token start

--- js/Dice.js
+++ js/Dice.js
@@ -3,13 +3,14 @@ function Dice(faces) {
 }
 
 Dice.prototype.initRender = function(container) {
-  var dice = $('<div>').text(this.faces);
-  dice.addClass('dice');
-  container.append(dice);
 
   var buttonRoll = $('<button>').text('ROLL DICE');
   buttonRoll.addClass("btn btn-default btn-lg roll");
   container.append(buttonRoll);
+
+  var dice = $('<div>').text(this.faces);
+  dice.addClass('dice');
+  container.append(dice);
 };
 
 Dice.prototype.roll = function() {
